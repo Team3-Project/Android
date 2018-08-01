8fca1ddcef7ab8b9ce76dc11cee63307faaba60a
BFELFISH <minwoo20101649@163.com>
2018-08-01 12:14:19 +0800

    增加登录界面

---
+++ app/src/main/java/com/example/team3/wens_farm/Log_In.java
@@ -0,0 +1,13 @@
+package com.example.team3.wens_farm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Log_In extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log__in);
+    }
+}
